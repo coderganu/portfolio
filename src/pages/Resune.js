49bbdd889a4a1a601645e408473abb0cbf9ece03
coderganu <daiveshsuryawanshi@hotmail.com>
2022-02-05 23:18:34 +0530

    build

--- src/pages/Resune.js
+++ src/pages/Resune.js
@@ -9,7 +9,7 @@ const Resume = () => {
 <div className="container-fluid">
 
 
-<iframe src="https://drive.google.com/file/d/1OK0Xj6JNbfxb4lAF0pu-lf7cQGdpfB4m/preview" width="100%" height="680" allow="autoplay"></iframe>
+<iframe src="https://drive.google.com/file/d/1xxjLGeKur5esN26OYVcsMuBZAE7-kZ6r/preview" width="100%" height="680" allow="autoplay"></iframe>
 </div>
 
         </>
